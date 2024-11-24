c5300d4c4b5c1e9dcbecf294f8c099c27b448dc5
DerickNorlan <dnorlan01@student.ciputra.ac.id>
2024-11-24 16:09:04 +0800

    onboard + login

---
+++ peduliPanti_login/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'onboarding1.dart'; // Import file onboarding1.dart
+
+void main() {
+  runApp(Onboarding1App()); // Panggil kelas OnboardingApp dari onboarding1.dart
+}
