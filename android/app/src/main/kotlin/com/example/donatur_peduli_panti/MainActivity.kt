057269b6984f7e2604d0e42402ddc98e74697e9e
Alvin Yuga Pramana <148262832+bigbosspramana@users.noreply.github.com>
2024-12-04 22:17:51 +0800

    Menambahkan beberapa folder

---
+++ android/app/src/main/kotlin/com/example/donatur_peduli_panti/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.donatur_peduli_panti
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:donatur_peduli_panti/regist.dart';
+
+void main() {
+  runApp(const RegistApp());
+}
