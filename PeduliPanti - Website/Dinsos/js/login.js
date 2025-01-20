58ae06197b99d74c84582cdc426720078e03c774
Muhammad Habbibie Zikrillah <167674577+habibiezkrillh@users.noreply.github.com>
2025-01-20 09:33:10 +0800

    minor changes javascript

--- PeduliPanti - Website/Dinsos/js/login.js
+++ PeduliPanti - Website/Dinsos/js/login.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         try {
-            const response = await fetch("https://127.0.0.1:8000/api/v1", {
+            const response = await fetch("http://127.0.0.1:8000/api/v1", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
